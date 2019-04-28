a727a7cd7bf03affa7b21a09e2e5c1fa4b158dcb
Paolla Dutra <paolladutra@nyu.edu>
2019-04-27 20:33:49 -0400

    Assignment8

--- Assignment8/script.js
+++ Assignment8/script.js
@@ -1,13 +1,12 @@
 var img = document.getElementById("bone")
 
+
 function showImg(event)
 { 
-	var x = event.clientX;
-	var y = event.clientY;
-	//document.getElementById("bone").style.left = x+"px";
-	// document.getElementById("bone").style.top = y+"px";
-	// document.getElementById("bone").replace(x, y);
-	img.style.display="block"; 
-	img.style.left = x + 'px';
-	img.style.top = y + 'px';
+	var x = parseInt(event.clientX);
+	var y = parseInt(event.clientY);
+	img.style.display="block";
+	img.style.position = 'absolute';
+	img.style.left  = x + 'px'; 
+	img.style.top  =  y + 'px'; 
 } 
